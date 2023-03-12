e8c015273035b85b8333c6f52a57e73a4369f84e
Adriano Souza <ah.driano@gmail.com>
2023-03-12 18:58:09 -0400

    chore: mobile version

---
+++ script.js
@@ -0,0 +1,57 @@
+const gradients = [
+  {
+    odd: '#80FFEA',
+    even: '#8AFF80',
+  },
+  {
+    odd: '#FF80BF',
+    even: '#9580FF',
+  },
+  {
+    odd: '#9442FE',
+    even: '#3378FF',
+  },
+  {
+    odd: '#9580FF',
+    even: '#80FFEA',
+  },
+];
+
+function rotateLogoColor(index) {
+  const oddColors = document.getElementsByClassName('logo-color-odd');
+  const evenColors = document.getElementsByClassName('logo-color-even');
+
+  setTimeout(() => {
+    for (const color of oddColors) {
+        color.style.setProperty('stop-color', gradients[index].odd);
+      }
+      for (const color of evenColors) {
+        color.style.setProperty('stop-color', gradients[index].even);
+      }
+
+      index = index === gradients.length - 1 ? 0 : index + 1;
+      rotateLogoColor(index)
+  }, 7000);
+}
+
+function setupFaqControllers() {
+  const controllers = document.getElementsByClassName('controller');
+
+  for (const controller of controllers) {
+
+    controller.addEventListener('click', () => {
+      const state = controller.dataset.state;
+      const rotation = state === 'closed' ? 'rotate(180deg)' : 'rotate(0)'
+      const display = state === 'closed' ? 'block' : 'none';
+
+      controller.style.setProperty('transform', rotation);
+      controller.dataset.state = state === 'closed' ? 'open' : 'closed';
+
+      const answer = controller.parentElement.parentElement.getElementsByClassName('answer')[0];
+      answer.style.setProperty('display', display);
+    });
+  }
+}
+
+rotateLogoColor(0);
+setupFaqControllers();
